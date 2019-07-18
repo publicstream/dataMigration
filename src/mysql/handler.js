80b47df1c99b137115ca7730269acec8d224e4a0
Vikas <vikkycorner@gmail.com>
2019-07-19 00:25:49 +0530

    Added nodemon and wrapHandler for request and response

--- src/mysql/handler.js
+++ src/mysql/handler.js
@@ -1,20 +1,21 @@
 const mySql = require('../services/mysql');
+const { OK } = require('../requestResponseWrapper');
 
-function createConnection(req) {
-    var { host, user, password, db } = req.request.body;
+function createConnection(body) {
+    var { host, user, password, db } = body;
     mySql.createConnection(host, user, password, db);
-    req.body={msg:"Connection created"};
+    return OK({msg:"Connection created"});
 }
 
 async function getTables(req) {
     let tables = await mySql.getTables()
-    req.body=tables;
+    return OK(tables);
 }
 
 async function getColumns(req) {
     let tableName = req.params.table;
     let columns = await mySql.getColumns(tableName);
-    req.body = columns;
+    return OK(columns);
 }
 
 
--- src/router.js
+++ src/router.js
@@ -1,8 +1,9 @@
 const Router = require('koa-router');
-const mySqlHandler = require('./mysql/handler');
+const { wrapHandlerModule } = require('./requestResponseWrapper');
+const mySqlHandler = wrapHandlerModule(require('./mysql/handler'));
 const router = new Router({ prefix: '/' });
 
-router.post('mysql/createConnection', mySqlHandler.createConnection);
+router.post('mysql/createConnection', mySqlHandler.createConnection) ;
 router.get('mysql/getTables', mySqlHandler.getTables);
 router.get('mysql/getColumns/:table', mySqlHandler.getColumns);
 
